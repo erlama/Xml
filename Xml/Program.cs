88c7cca41119ecdbeffe653c00ad530f7a144867
Erla Edvadsdottir <erlama85@gmail.com>
2018-10-10 12:33:55 +0200

    Add project files.

---
+++ Xml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
